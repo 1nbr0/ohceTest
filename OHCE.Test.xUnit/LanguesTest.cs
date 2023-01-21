ac495856ecbe41169d3ca2029e529593878c35eb
Robin667 <robin.turpin8@gmail.com>
2023-01-21 14:05:12 +0100

    ajout test auRevoir langue anglaise + moments + ajout complmentaire test langue française

--- OHCE.Test.xUnit/LanguesTest.cs
+++ OHCE.Test.xUnit/LanguesTest.cs
@@ -52,11 +52,35 @@ namespace OHCE.Test.xUnit
                           "QUAND l'app démarre " +
                           "ALORS <auRevoir> de cette langue à ce moment est envoyé en dernier")]
         [InlineData(MomentJournee.Matin, Expressions.Français.AuRevoir)]
+        [InlineData(MomentJournee.AprèsMidi, Expressions.Français.AuRevoir)]
+        [InlineData(MomentJournee.Soir, Expressions.Français.AuRevoir)]
+        [InlineData(MomentJournee.Nuit, Expressions.Français.AuRevoir)]
         public void DireAuRevoirTestFrancais(MomentJournee moment, string AuRevoirAttendue)
         {
             // ETANT DONNE un utilisateur parlant la langue française
             // ET que le moment de la journée est <moment>
             var langue = new LangueFrancaise();
+
+            // QUAND l'app démarre
+            var auRevoir = langue.AuRevoirBis(moment);
+
+            // ALORS <salutationAttendue> de cette langue à ce moment est envoyé avant tout
+            Assert.Equal(AuRevoirAttendue, auRevoir);
+        }
+
+        [Theory(DisplayName = "ETANT DONNE un utilisateur parlant une langue " +
+                          "ET que le moment de la journée est <moment> " +
+                          "QUAND l'app démarre " +
+                          "ALORS <auRevoir> de cette langue à ce moment est envoyé en dernier")]
+        [InlineData(MomentJournee.Matin, Expressions.English.AuRevoir)]
+        [InlineData(MomentJournee.AprèsMidi, Expressions.English.AuRevoir)]
+        [InlineData(MomentJournee.Soir, Expressions.English.AuRevoir)]
+        [InlineData(MomentJournee.Nuit, Expressions.English.AuRevoir)]
+        public void DireAuRevoirTestAnglais(MomentJournee moment, string AuRevoirAttendue)
+        {
+            // ETANT DONNE un utilisateur parlant la langue française
+            // ET que le moment de la journée est <moment>
+            var langue = new LangueAnglaise();
 
             // QUAND l'app démarre
             var auRevoir = langue.AuRevoirBis(moment);
